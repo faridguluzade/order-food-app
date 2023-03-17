d2589f18b7e0baf74f6c85242a451cb9a1715a83
Farid <faridguluzade26@gmail.com>
2023-03-16 23:52:52 -0800

    initial commit

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,19 @@
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+
+const MealItem = ({ id, name, description, price }) => {
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>${price.toFixed(2)}</div>
+      </div>
+      <div>
+        <MealItemForm id={id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
